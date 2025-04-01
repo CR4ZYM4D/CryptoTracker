cfa4b683cc00c38c0e0ad5bc91014209e6073c4a
Madhav Dixit <madhavdixit482@gmail.com>
2025-04-02 01:37:14 +0530

    chart completed integration left

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -2,6 +2,7 @@ package com.plcoding.cryptotracker.crypto.presentation.coin_detail
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -17,6 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.draw
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
@@ -29,6 +34,7 @@ import com.plcoding.cryptotracker.crypto.domain.CoinPrice
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 @Composable
@@ -82,7 +88,21 @@ fun LineChart(
     }
 
     Canvas(modifier = modifier
-        .fillMaxSize()) {
+        .fillMaxSize()
+        .pointerInput(drawPoints , xLabelWidth){
+            detectHorizontalDragGestures { change, _ ->
+                val newSelectedDataPointIndex = getSelectedCoin(touchOffsetX = change.position.x,
+                    triggerWidth = xLabelWidth ,
+                    drawPoints = drawPoints)
+
+                isShowingDataPoints = (newSelectedDataPointIndex + visiblePointIndices.first()) in
+                        visiblePointIndices
+
+                if(isShowingDataPoints){
+                    onSelectedDataPoint(dataPoints[newSelectedDataPointIndex])
+                }
+            }
+        }) {
 
         val minYLabelSpacingPx = chartStyle.minYLabelSpacing.toPx()
         val verticalPaddingPx = chartStyle.verticalPadding.toPx()
@@ -182,7 +202,6 @@ fun LineChart(
                 ),
                 color = if(index == selectedDataPointIndex) chartStyle.selectedColor
                     else chartStyle.unselectedColor
-
             )
             if(showGridLines) {
                 drawLine(
@@ -193,13 +212,126 @@ fun LineChart(
                         x = x+result.size.width/2,
                         y = viewPortTopY
                     ),
-                    strokeWidth = chartStyle.gridLineThickness
+                    strokeWidth = if(index == selectedDataPointIndex) chartStyle.gridLineThickness*2.25f
+                    else chartStyle.gridLineThickness
                 )
+            }
+
+            if(selectedDataPointIndex == index){
+                val valueLabel = ValueLabel(
+                    value = visibleDataPoints[index].y,
+                    unit = unit
+                )
+                val valueResult = remember.measure(
+                    text = valueLabel.format(),
+                    style = textStyle.copy(color = chartStyle.selectedColor),
+                    maxLines = 1
+                )
+                val textPositionX = if(selectedDataPointIndex == visiblePointIndices.last) {
+                    x - valueResult.size.width }
+                else { x - valueResult.size.width / 2 } + result.size.width/2
+
+                val isTextInVisibleRange = (size.width - textPositionX).roundToInt() in
+                        0..size.width.roundToInt()
+
+                if(isTextInVisibleRange){
+                    drawText(
+                        textLayoutResult = valueResult,
+                        topLeft = Offset(
+                            x = textPositionX,
+                            y = viewPortTopY - valueResult.size.height
+                        )
+                    )
+                }
             }
         }
 
+        drawPoints = visiblePointIndices.map{
+            val x = viewPortLeftX -
+                    xLabelSpacingPx/3 +
+                    xLabelWidth*(it - visiblePointIndices.first)+
+                    xLabelWidth/2f
+            val ratio = (dataPoints[it].y - minYValue)/ (maxYValue - minYValue)
+            val y = viewPortBottomY - ratio*viewPortPxHeight
+            DataPoint(
+                x = x,
+                y = y,
+                xLabel = dataPoints[it].xLabel
+            )
+        }
 
+        val bezierPoints1= mutableListOf<DataPoint>()
+        val bezierPoints2= mutableListOf<DataPoint>()
+
+        for (i in 1 until drawPoints.size){
+
+            val p0 = drawPoints[i-1]
+            val p1 = drawPoints[i]
+
+            val x = (p1.x + p0.x)/2
+            val y0 = p0.y
+            val y1 = p1.y
+
+            bezierPoints1.add(DataPoint(x, y0 , ""))
+            bezierPoints2.add(DataPoint(x, y1 , ""))
+
+        }
+
+        val linePath = Path().apply{
+
+            if(drawPoints.isNotEmpty()){
+                moveTo(drawPoints.first().x , drawPoints.first().y)
+                for(i in 1 until drawPoints.size){
+                    cubicTo(
+                        x1 = bezierPoints1[i-1].x,
+                        y1 = bezierPoints1[i-1].y,
+                        x2 = bezierPoints2[i-1].x,
+                        y2 = bezierPoints2[i-1].y,
+                        x3 = drawPoints[i].x,
+                        y3 = drawPoints[i].y
+                    )
+                }
+            }
+
+        }
+
+        drawPath(path = linePath ,
+            color = chartStyle.chartLineColor,
+            style = Stroke(width = 2f))
+
+        drawPoints.forEachIndexed { it, drawPoint ->
+
+            if(isShowingDataPoints) {
+                drawCircle(
+                    color = chartStyle.unselectedColor,
+                    radius = chartStyle.gridLineThickness * 10,
+                    center = Offset(x = drawPoint.x, y = drawPoint.y)
+                )
+
+                if(selectedDataPointIndex == it){
+                    drawCircle(
+                        color =Color.White,
+                        radius = chartStyle.gridLineThickness * 12,
+                        center = Offset(x = drawPoint.x, y = drawPoint.y)
+                    )
+                    drawCircle(
+                        color = chartStyle.selectedColor,
+                        radius = chartStyle.gridLineThickness * 12,
+                        center = Offset(x = drawPoint.x, y = drawPoint.y),
+                        style = Stroke(width = 3f)
+                    )
+                }
+            }
+        }
     }
+
+}
+
+private fun getSelectedCoin(touchOffsetX: Float, triggerWidth:Float , drawPoints:List<DataPoint>):Int{
+
+    val triggerRangeLeft = touchOffsetX - triggerWidth/2
+    val triggerRangeRight = touchOffsetX + triggerWidth/2
+    return drawPoints.indexOfFirst {    it.x in triggerRangeLeft..triggerRangeRight   }
 
 }
 
@@ -219,11 +351,11 @@ fun lineChart(){
         }
 
         val chartStyle = ChartStyle(
-            chartLineColor = Color.Black,
-            unselectedColor = Color.Gray,
+            chartLineColor = Color.Gray,
+            unselectedColor = Color.LightGray,
             selectedColor = Color.Magenta,
-            gridLineThickness = 3f,
-            axisLineThickness = 2f,
+            gridLineThickness = 1f,
+            axisLineThickness = 1f,
             labelFontSize = 12.sp,
             minYLabelSpacing = 12.dp,
             verticalPadding = 10.dp,
